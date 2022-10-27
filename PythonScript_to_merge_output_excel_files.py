4ff6c684afecc46dc77050cad5ef32c10bb20167
KartikeyaReddy24 <79633450+KartikeyaReddy24@users.noreply.github.com>
2022-10-27 00:34:39 -0400

    Updated few corrections

--- PythonScript_to_merge_output_excel_files.py
+++ PythonScript_to_merge_output_excel_files.py
@@ -7,10 +7,10 @@ data_file_folder='/home/ubuntu/excel'
 df = []
 
 for file in os.listdir(data_file_folder):
-    if file.endswith('.xlsh'):
+    if file.endswith('.xlsx'):
         print('Loading file {0}...'.format(file))
         df.append(pd.read_excel(os.path.join(data_file_folder, file), sheet_name='Emails'))
 
 
 df_master= pd.concat(df, axis=0)
-df_master.to_excel('master_file.xlsh', index=False)
+df_master.to_excel('master_file.xlsx', index=False)
